1e4a570b29f263f9fdf4dc1c1d63a77338167961
Athira E <athiaisutvnr@gmail.com>
2023-07-08 18:09:44 +0530

    updated countdown

--- js/main.js
+++ js/main.js
@@ -59,7 +59,7 @@ jQuery(function ($) {
 
    if ($('.countdown').length > 0) {
       $(".countdown").jCounter({
-         date: '10 July 2023 12:00:00',
+         date: '15 August 2023 24:00:00',
          fallback: function () {
             console.log("count finished!")
          }
